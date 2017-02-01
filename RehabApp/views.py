3df626740936ab8f7e78b5a8a0b8b4b299e00ecf
gkafka <gareth.kafka@gmail.com>
2017-02-01 16:18:59 -0500

    Remove basemap implementation from app. Get county data from SQL db, have map display true-predicted value on a red blue scale.

--- RehabApp/views.py
+++ RehabApp/views.py
@@ -4,9 +4,11 @@ import StringIO
 from flask import render_template, request, send_file
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap
 import numpy as np
 import pandas as pd
+import psycopg2
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
 
 #from bokeh.io import show
 from bokeh import embed
@@ -15,7 +17,7 @@ from bokeh.models import (
     HoverTool,
     LinearColorMapper
 )
-from bokeh.palettes import Viridis6 as palette
+from bokeh.palettes import RdBu9 as palette
 from bokeh.plotting import figure, output_notebook, show
 
 from bokeh.sampledata.us_counties import data as countydata
@@ -94,37 +96,20 @@ def contact():
 def index():
     return render_template("index.html")
 
-@app.route('/map_basemap')
-def map_basemap():
-    fig, ax = plt.subplots(figsize=(15,9))
-
-    m = Basemap(resolution='l', # c, l, i, h, f or None
-                projection='lcc',
-                lat_1=33, lat_2=45, lon_0=-95,
-                llcrnrlon=-119, llcrnrlat=22, urcrnrlon=-64, urcrnrlat=49)
-
-    m.readshapefile('RehabApp/static/cb_2015_us_county_5m', 'County')
-
-    df = pd.DataFrame({
-            'shapes': [mpl.patches.Polygon(np.array(shape), True) for shape in m.County],
-            'area': [county['NAME'] for county in m.County_info]
-        })
-
-    cmap = plt.get_cmap('Oranges')
-    pc = mpl.collections.PatchCollection(df.shapes, zorder=2)
-    norm = mpl.colors.Normalize()
-
-    pc.set_facecolor(cmap(norm(np.random.rand(df.shape[0]))))
-    ax.add_collection(pc)
-    
-    img = StringIO.StringIO()
-    fig.savefig(img)
-    img.seek(0)
-    return send_file(img, mimetype='image/png')
+def fix_word(word):
+    new_word = word.lower().replace('.', '')
+    new_word = new_word.replace('saint','st')
+    return new_word
 
 def map():
-    palette.reverse()
+    user = 'gkafka' #add your username here (same as previous postgreSQL)
+    host = 'localhost'
+    dbname = 'rehab_db'
 
+    db = create_engine('postgres://%s%s/%s'%(user,host,dbname))
+    con = None
+    con = psycopg2.connect(database = dbname, user = user)
+    
     counties = {
         code: county for code, county in countydata.items() if (county["state"] != "ak" and county['state'] != "hi")
         #if county['state'] == 'tx'
@@ -132,9 +117,22 @@ def map():
 
     county_xs = [county["lons"] for county in counties.values()]
     county_ys = [county["lats"] for county in counties.values()]
-
     county_names = [county['name'] for county in counties.values()]
-    county_rates = np.random.rand(len(counties))
+
+    sql_query = """
+                SELECT county, st, pred_diff FROM rehab_table WHERE year=2014;
+                """
+    df = pd.read_sql_query(sql_query,con)
+    
+    county_rates = []
+    for county in counties.values():
+        mask = ((df['county'] == fix_word(county['name'])) &
+                (df['st']     == county['state'].upper()))
+        if np.sum(mask) == 0:
+            county_rates.append(0)
+        else:
+            county_rates.append(df[mask]['pred_diff'].values[0])
+
     color_mapper = LinearColorMapper(palette=palette)
     
     source = ColumnDataSource(data=dict(
@@ -147,7 +145,7 @@ def map():
     TOOLS = "pan,box_zoom,reset,hover,save"
 
     p = figure(
-        title="Texas Unemployment, 2009", tools=TOOLS,
+        title="Rehab Facility Targets, US Counties", tools=TOOLS,
         x_axis_location=None, y_axis_location=None
     )
     p.grid.grid_line_color = None
@@ -159,8 +157,8 @@ def map():
     hover = p.select_one(HoverTool)
     hover.point_policy = "follow_mouse"
     hover.tooltips = [
-        ("Name", "@name"),
-        ("Unemployment rate)", "@rate"),
+        ("County", "@name"),
+        ("True - Predicted Rate", "@rate"),
         ("(Long, Lat)", "($x, $y)"),
     ]
 
