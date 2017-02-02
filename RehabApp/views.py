628bda4460c0dc2e091802f21eb403d52b96ed27
gkafka <gareth.kafka@gmail.com>
2017-02-02 15:56:20 -0500

    Plot map in d3, not bokeh.

--- RehabApp/views.py
+++ RehabApp/views.py
@@ -7,18 +7,6 @@ import psycopg2
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 
-#from bokeh.io import show
-from bokeh import embed
-from bokeh.models import (
-    ColumnDataSource,
-    HoverTool,
-    LinearColorMapper
-)
-from bokeh.palettes import RdBu9 as palette
-from bokeh.plotting import figure, output_notebook, show
-
-from bokeh.sampledata.us_counties import data as countydata
-
 user = 'gkafka'
 host = 'localhost'
 dbname = 'rehab_db'
@@ -26,9 +14,6 @@ dbname = 'rehab_db'
 @app.route('/')
 @app.route('/home')
 def get_model_input():
-    plot = map()
-    script, div = embed.components(plot)
-
     db = create_engine('postgres://%s%s/%s'%(user,host,dbname))
     con = None
     con = psycopg2.connect(database = dbname, user = user)
@@ -43,8 +28,7 @@ def get_model_input():
     for i in xrange(df.shape[0]):
         menu_entries.append("{0}, {1}".format(df.iloc[i]['st'], df.iloc[i]['county'].upper()))
 
-    return render_template("home_map.html",
-        script=script, div=div, menu_entries=menu_entries)
+    return render_template("home_map.html", menu_entries=menu_entries)
 
 @app.route('/explorer')
 def explore():
@@ -164,67 +148,3 @@ def contact():
 @app.route('/index')
 def index():
     return render_template("index.html")
-
-def fix_word(word):
-    new_word = word.lower().replace('.', '')
-    new_word = new_word.replace('saint','st')
-    return new_word
-
-def map():
-    db = create_engine('postgres://%s%s/%s'%(user,host,dbname))
-    con = None
-    con = psycopg2.connect(database = dbname, user = user)
-    
-    counties = {
-        code: county for code, county in countydata.items() if (county["state"] != "ak" and county['state'] != "hi")
-        #if county['state'] == 'tx'
-    }
-
-    county_xs = [county["lons"] for county in counties.values()]
-    county_ys = [county["lats"] for county in counties.values()]
-    county_names = [county['name'] for county in counties.values()]
-
-    sql_query = """
-                SELECT county, st, pred_diff FROM rehab_table WHERE year=2014;
-                """
-    df = pd.read_sql_query(sql_query,con)
-    
-    county_rates = []
-    for county in counties.values():
-        mask = ((df['county'] == fix_word(county['name'])) &
-                (df['st']     == county['state'].upper()))
-        if np.sum(mask) == 0:
-            county_rates.append(0)
-        else:
-            county_rates.append(df[mask]['pred_diff'].values[0])
-
-    color_mapper = LinearColorMapper(palette=palette)
-    
-    source = ColumnDataSource(data=dict(
-        x=county_xs,
-        y=county_ys,
-        name=county_names,
-        rate=county_rates,
-    ))
-
-    TOOLS = "pan,box_zoom,reset,hover,save"
-
-    p = figure(
-        title="Rehab Facility Targets, US Counties", tools=TOOLS,
-        x_axis_location=None, y_axis_location=None
-    )
-    p.grid.grid_line_color = None
-
-    p.patches('x', 'y', source=source,
-              fill_color={'field': 'rate', 'transform': color_mapper},
-              fill_alpha=0.7, line_color="white", line_width=0.5)
-
-    hover = p.select_one(HoverTool)
-    hover.point_policy = "follow_mouse"
-    hover.tooltips = [
-        ("County", "@name"),
-        ("True - Predicted Rate", "@rate"),
-        ("(Long, Lat)", "($x, $y)"),
-    ]
-
-    return p
